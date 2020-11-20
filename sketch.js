7d6735088f06765198760281e27c0ff709b5af28
januwA <ajanuw1995@gmail.com>
2020-11-20 12:46:27 +0800

    优化下载

--- sketch.js
+++ sketch.js
@@ -1,20 +1,18 @@
-let canvas;
+let p5Canvas;
 
 // 1
-let basicImg;
+let basicImg; // 上传的原始图片
 let basicBox;
 let fileInputEle;
 
 // 2
 let textareaEle;
 let sizes = [];
-let textareaInitValue = `
-48x48
+let textareaInitValue = `48x48
 72x72
 96x96
 144x144
-192x192
-`;
+192x192`;
 
 // 3
 let showBtn;
@@ -23,11 +21,12 @@ let imgs = [];
 
 // 4
 let downloadBtn;
+let downloadFlutterBtn;
 
 function setup() {
-  canvas = createCanvas(200, 200);
+  p5Canvas = createCanvas(200, 200);
   background(0);
-  canvas.hide();
+  p5Canvas.hide();
 
   createElement("h2", "1. 上传图片");
   fileInputEle = createFileInput(gotFile);
@@ -45,17 +44,32 @@ function setup() {
 
   createElement("h2", "4. 下载");
   downloadBtn = createButton("Downlaod");
-  downloadBtn.mouseClicked(startDownload);
+  downloadBtn.mouseClicked(function () {
+    if (!imgs.length) {
+      return alert("先预览!!");
+    }
+    drawDown(imgs);
+  });
+
+  downloadFlutterBtn = createButton("Downlaod/Flutter");
+  downloadFlutterBtn.mouseClicked(downloadFlutterLauncher);
 }
 
-/**
- * 点击下载事件
- */
-function startDownload() {
-  if (!imgs.length) {
-    return alert("先预览!!");
-  }
-  drawDown(imgs);
+async function toFlutterLauncherPng(hDir, index) {
+  const hImageFile = await hDir.getFileHandle("ic_launcher.png", {
+    create: true,
+  });
+  const { w, h } = imgs[index];
+  const img = createImg(basicImg.data, async () => {
+    resizeCanvas(w, h);
+    image(img, 0, 0, w, h);
+    p5Canvas.canvas.toBlob(async function (blob) {
+      const w$ = await hImageFile.createWritable();
+      await w$.write(blob);
+      await w$.close();
+    });
+  });
+  img.hide();
 }
 
 /**
@@ -64,15 +78,15 @@ function startDownload() {
  */
 function drawDown(images) {
   if (images.length === 0) return false;
-  let { data, w, h } = images[0];
-  let img = createImg(data, () => {
+  let { w, h } = images[0];
+  let img = createImg(basicImg.data, () => {
     resizeCanvas(w, h);
     image(img, 0, 0, w, h);
     const name = `${w}x${h}_${basicImg.name}`;
     let dbtn = createA("javascript: (void 0)", name);
-    dbtn.mouseClicked(e => {
+    dbtn.mouseClicked((e) => {
       if (!img) return null;
-      const dataURL = canvas.canvas.toDataURL(basicImg.file.type);
+      const dataURL = p5Canvas.canvas.toDataURL(basicImg.file.type);
       dbtn.attribute("download", name);
       dbtn.attribute("href", dataURL);
     });
@@ -102,12 +116,11 @@ function showClicked() {
     const [_, w, h] = /(^\d+)\D+(\d+$)/.exec(s);
     let li = createElement("li", "").parent(showBox);
     createP(s).parent(li);
-    imgs.push({ data: basicImg.data, w, h });
+    imgs.push({ w, h });
     let img = createImg(basicImg.data, () => {
       img.parent(li);
       img.attribute("width", w);
       img.attribute("height", h);
-      image(img);
     });
   }
 }
@@ -136,4 +149,47 @@ function gotFile(file) {
     // image(img, 0, 0, 200, 200);
   });
   img.size(400, 400);
+}
+
+async function downloadFlutterLauncher() {
+  if (!imgs.length) {
+    return alert("先预览!!");
+  }
+
+  if (!window.showDirectoryPicker) {
+    return alert("not showDirectoryPicker API");
+  }
+
+  // 获取目录句柄
+  const hSelectDir = await showDirectoryPicker();
+
+  // 创建新目录，如果存在直接覆盖
+  const hNewDir = await hSelectDir.getDirectoryHandle("launcher-res", {
+    create: true,
+  });
+
+  let hDir = await hNewDir.getDirectoryHandle("mipmap-mdpi", {
+    create: true,
+  });
+  await toFlutterLauncherPng(hDir, 0);
+
+  hDir = await hNewDir.getDirectoryHandle("mipmap-hdpi", {
+    create: true,
+  });
+  await toFlutterLauncherPng(hDir, 1);
+
+  hDir = await hNewDir.getDirectoryHandle("mipmap-xhdpi", {
+    create: true,
+  });
+  await toFlutterLauncherPng(hDir, 2);
+
+  hDir = await hNewDir.getDirectoryHandle("mipmap-xxhdpi", {
+    create: true,
+  });
+  await toFlutterLauncherPng(hDir, 3);
+
+  hDir = await hNewDir.getDirectoryHandle("mipmap-xxxhdpi", {
+    create: true,
+  });
+  await toFlutterLauncherPng(hDir, 4);
 }
